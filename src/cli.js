3b9cf5da549b50df148afeec048e63a5c77c4719
Jeffrey Moon <fourleaves@fourleaves.io>
2019-12-21 13:54:36 -0500

    sdf

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,35 @@
-var fileScanner = require("./file-scanner");
+import { readFileSync } from 'fs';
+import readdir from 'recursive-readdir';
+import { extname } from 'path';
 
-fileScanner.getAllDirectoryFiles();
+const exts = [
+  '.cc',
+  '.cpp',
+  '.c',
+  '.cxx',
+  '.h',
+  '.hpp'
+];
+
+function readConfig() {
+  return JSON.parse(readFileSync('autocmake.json'));
+}
+
+function getAllDirectoryFiles() {
+  readdir('.', [fileSelector], (err, files) => {
+    console.log(files);
+  });
+}
+
+function fileSelector(file, stats) {
+  const ext = extname(file);
+  return !(exts.includes(ext));
+}
+
+
+export default {
+  readConfig,
+  getAllDirectoryFiles
+}
+
+getAllDirectoryFiles();
